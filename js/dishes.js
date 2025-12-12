d799ebdb763317f5ca244b967e61893283cf9ff1
Mischa Siegrist <therealphantom22@proton.me>
2025-12-12 08:39:03 +0100

    added small db for dishes

---
+++ js/dishes.js
@@ -0,0 +1,32 @@
+let dishes = [
+    {
+        "name":"Hamburger",
+        "description":"Saftiges Rindfleisch Patty, gegrillt bis zur perfekten Bräune, serviert auf einem weichen Brioche Brötchen mit knackigem Salat, Tomate, Zwiebel und unserer hausgemachten Sauce.",
+        "imgSrc":"./assets/img/hamburger.jpg"
+    },
+    {
+        "name":"Cheese Burger",
+        "description":"Saftiges Rindfleisch Patty, überbacken mit geschmolzenem Cheddar, serviert auf einem leicht gerösteten Sesambrötchen mit Salat, Tomate, Zwiebel und unserer würzigen Burger Sauce.",
+        "imgSrc":"./assets/img/cheeseburger.jpg"
+    },
+    {
+        "name":"Vegi Burger",
+        "description":"Herzhaftes Gemüse- und Kichererbsen-Patty, gewürzt mit mediterranen Kräutern, belegt mit Avocado, Rucola und einer leichten Joghurt-Tahini-Sauce auf einem Vollkornbrötchen.",
+        "imgSrc":"./assets/img/vegi-burger.jpg"
+    },
+    {
+        "name":"Pizza Margherita",
+        "description":"Dünner, knuspriger Boden, bestrichen mit aromatischer San-Marzano-Tomatensauce, belegt mit cremigem Büffelmozzarella und frischem Basilikum, nach dem Backen mit einem Hauch Olivenöl veredelt.",
+        "imgSrc":"./assets/img/pizza-margherita.jpg"
+    },
+    {
+        "name":"Caesar Salat",
+        "description":"Frischer Römersalat, knusprige Croutons und Parmesanflocken, alles vereint in einer cremigen Caesar-Dressing, optional garniert mit gegrillten Hähnchenstreifen.",
+        "imgSrc":"./assets/img/cesar-salad.jpg"
+    },
+    {
+        "name":"Ramen Noodels",
+        "description":"Duftende, handgezogene Weizennudeln in einer klaren, langsam gekochten Brühe, ergänzt durch zarte Scheiben Schweinebauch, Frühlingszwiebeln, ein weich gekochtes Ei und Algenstreifen.",
+        "imgSrc":"./assets/img/ramen.jpg"
+    }
+]
