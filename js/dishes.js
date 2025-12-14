d2baf8c8421a3fefc93edfd4850cdcc08b12a2a8
Mischa Siegrist <therealphantom22@proton.me>
2025-12-14 13:20:37 +0100

    added id to dishes

--- js/dishes.js
+++ js/dishes.js
@@ -1,35 +1,41 @@
 let dishes = [
     {
+        "id":0,
         "name":"Hamburger",
         "price":"6.40",
         "description":"Saftiges Rindfleisch Patty, gegrillt bis zur perfekten Bräune, serviert auf einem weichen Brioche Brötchen mit knackigem Salat, Tomate, Zwiebel und unserer hausgemachten Sauce.",
         "imgSrc":"./assets/img/hamburger.jpg"
     },
     {
+        "id":1,
         "name":"Cheese Burger",
         "price":"5.20",
         "description":"Saftiges Rindfleisch Patty, überbacken mit geschmolzenem Cheddar, serviert auf einem leicht gerösteten Sesambrötchen mit Salat, Tomate, Zwiebel und unserer würzigen Burger Sauce.",
         "imgSrc":"./assets/img/cheeseburger.jpg"
     },
     {
+        "id":2,
         "name":"Vegi Burger",
         "price":"5.80",
         "description":"Herzhaftes Gemüse- und Kichererbsen-Patty, gewürzt mit mediterranen Kräutern, belegt mit Avocado, Rucola und einer leichten Joghurt-Tahini-Sauce auf einem Vollkornbrötchen.",
         "imgSrc":"./assets/img/vegi-burger.jpg"
     },
     {
+        "id":3,
         "name":"Pizza Margherita",
         "price":"17.00",
         "description":"Dünner, knuspriger Boden, bestrichen mit aromatischer San-Marzano-Tomatensauce, belegt mit cremigem Büffelmozzarella und frischem Basilikum, nach dem Backen mit einem Hauch Olivenöl veredelt.",
         "imgSrc":"./assets/img/pizza-margherita.jpg"
     },
     {
+        "id":4,
         "name":"Caesar Salat",
         "price":"12.50",
         "description":"Frischer Römersalat, knusprige Croutons und Parmesanflocken, alles vereint in einer cremigen Caesar-Dressing, optional garniert mit gegrillten Hähnchenstreifen.",
         "imgSrc":"./assets/img/cesar-salad.jpg"
     },
     {
+        "id":5,
         "name":"Ramen Noodels",
         "price":"8.95",
         "description":"Duftende, handgezogene Weizennudeln in einer klaren, langsam gekochten Brühe, ergänzt durch zarte Scheiben Schweinebauch, Frühlingszwiebeln, ein weich gekochtes Ei und Algenstreifen.",
