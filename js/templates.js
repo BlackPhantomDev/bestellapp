acc8c690082d1837aa4ffc2e5c13d53fc1952ac7
Mischa Siegrist <therealphantom22@proton.me>
2025-12-20 11:01:23 +0100

    - new label total - new label delivery cost - changes of order in new cart item

--- js/templates.js
+++ js/templates.js
@@ -26,18 +26,23 @@ function getCartBasketTemplate() {
             <table class="cart-table">
                 <thead>
                     <tr>
-                        <th>Menge</th>
-                        <th>Artikel</th>
-                        <th>Preis</th>
+                        <th class="cart-dish-name">Artikel</th>
+                        <th class="cart-item-counter">Menge</th>
+                        <th class="cart-price-tag">Preis</th>
                     </tr>
                 </thead>
                 <tbody id="cart_table_content">
                     
                 </tbody>
-            </table>
-            <h4>Total:</h4>
+                </table>
         </div>
-        <button id="checkout_btn">Zum Checkout</button>
+        <div id="checkout">
+            <span>Versand: 5.90 CHF</span><h4>Total: <span>${totalPrice.toFixed(2)} CHF</span></h4>
+            <div id="cart_btns_container">
+                <button class="cart_btn" id="checkout_btn">Zum Checkout</button>
+                <button class="cart_btn" id="clear_cart_btn" onclick="clearCart()">Warenkorb leeren</button>
+            </div>
+        </div>
     </div>
     `;
 }
@@ -46,13 +51,13 @@ function getNewCartItem(id, name, amount, basePrice) {
     let displayedPrice = basePrice * amount;
     return `
         <tr>
+            <td class="cart-dish-name">${name}</td>
             <td class="cart-item-counter">
-                <button class="basket-btn" onclick="removeAmount(${id})">-</button>
-                <span>${amount}x</span>
-                <button class="basket-btn" onclick="addAmount(${id})">+</button>
+                <button class="basket-btn" onclick="changeAmount(${id}, 'remove')">-</button>
+                <span id="amount">${amount}x</span>
+                <button class="basket-btn" onclick="changeAmount(${id}, 'add')">+</button>
             </td>
-            <td class="item-name">${name}</td>
-            <td>${displayedPrice.toFixed(2)} CHF</td>
+            <td class="cart-price-tag">${displayedPrice.toFixed(2)} CHF</td>
         </tr>
     `;
 }
