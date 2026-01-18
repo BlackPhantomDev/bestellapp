4e905b54d86d60f3d26d322ba689419b7a6fb336
M. Siegrist <therealphantom22@proton.me>
2026-01-18 15:49:52 +0100

    added categories to dishes

--- js/dishes.js
+++ js/dishes.js
@@ -4,55 +4,63 @@ const dishes = [
         "name":"Hamburger",
         "price":"6.40",
         "description":"Saftiges Rindfleisch Patty, gegrillt bis zur perfekten Bräune, serviert auf einem weichen Brioche Brötchen mit knackigem Salat, Tomate, Zwiebel und unserer hausgemachten Sauce.",
-        "imgSrc":"./assets/img/hamburger.jpg"
+        "imgSrc":"./assets/img/hamburger.jpg",
+        "category":"main_dishes"
     },
     {
         "id":1,
         "name":"Cheese Burger",
         "price":"5.20",
         "description":"Saftiges Rindfleisch Patty, überbacken mit geschmolzenem Cheddar, serviert auf einem leicht gerösteten Sesambrötchen mit Salat, Tomate, Zwiebel und unserer würzigen Burger Sauce.",
-        "imgSrc":"./assets/img/cheeseburger.jpg"
+        "imgSrc":"./assets/img/cheeseburger.jpg",
+        "category":"main_dishes"
     },
     {
         "id":2,
         "name":"Vegi Burger",
         "price":"5.80",
         "description":"Herzhaftes Gemüse- und Kichererbsen-Patty, gewürzt mit mediterranen Kräutern, belegt mit Avocado, Rucola und einer leichten Joghurt-Tahini-Sauce auf einem Vollkornbrötchen.",
-        "imgSrc":"./assets/img/vegi-burger.jpg"
+        "imgSrc":"./assets/img/vegi-burger.jpg",
+        "category":"main_dishes"
     },
     {
         "id":3,
         "name":"Pizza Margherita",
         "price":"17.00",
         "description":"Dünner, knuspriger Boden, bestrichen mit aromatischer San-Marzano-Tomatensauce, belegt mit cremigem Büffelmozzarella und frischem Basilikum, nach dem Backen mit einem Hauch Olivenöl veredelt.",
-        "imgSrc":"./assets/img/pizza-margherita.jpg"
+        "imgSrc":"./assets/img/pizza-margherita.jpg",
+        "category":"main_dishes"
     },
     {
         "id":4,
         "name":"Caesar Salat",
         "price":"12.50",
         "description":"Frischer Römersalat, knusprige Croutons und Parmesanflocken, alles vereint in einer cremigen Caesar-Dressing, optional garniert mit gegrillten Hähnchenstreifen.",
-        "imgSrc":"./assets/img/cesar-salad.jpg"
+        "imgSrc":"./assets/img/cesar-salad.jpg",
+        "category":"appetizer"
     },
     {
         "id":5,
         "name":"Ramen Noodels",
         "price":"8.95",
         "description":"Duftende, handgezogene Weizennudeln in einer klaren, langsam gekochten Brühe, ergänzt durch zarte Scheiben Schweinebauch, Frühlingszwiebeln, ein weich gekochtes Ei und Algenstreifen.",
-        "imgSrc":"./assets/img/ramen.jpg"
+        "imgSrc":"./assets/img/ramen.jpg",
+        "category":"main_dishes"
     },
     {
         "id":6,
         "name":"Tiramisu",
         "price":"6.50",
         "description":"Ein Stück Italien? Bitte sehr. Der Klassiker unter den Italienischen Desserts neben dem Gelato. Zartes Kaffee-Schichtdessert mit weichem Mascarpone und Kakaopulver bestreut.",
-        "imgSrc":"./assets/img/tiramisu.jpeg"
+        "imgSrc":"./assets/img/tiramisu.jpeg",
+        "category":"dessert"
     },
     {
         "id":7,
         "name":"Käsekuchen &shy;(Quarktorte)",
         "price":"6.95",
         "description":"Geniesse unseren cremigen Käsekuchen – ein zarter, leicht säuerlicher Genuss, der auf einem knusprigen Mürbeteig ruht und mit einer dezenten Vanillenote vollendet wird. Perfekt als süsser Abschluss oder Begleitung zu einer Tasse Kaffee.",
-        "imgSrc":"./assets/img/kaesekuchen.jpg"
+        "imgSrc":"./assets/img/kaesekuchen.jpg",
+        "category":"dessert"
     }
 ]
